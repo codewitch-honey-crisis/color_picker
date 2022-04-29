f1de41acb15c13a68fc69f97a29f782d5d96adc7
honey the codewitch <gaztoast@gmail.com>
2022-04-29 06:42:25 -0700

    updated to include grayscale

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <ili9341.hpp>
 // touch header
 #include <xpt2046.hpp>
+
 // gfx for C++14 header
 #include <gfx_cpp14.hpp>
 // our x11 stuff
@@ -39,14 +40,17 @@ using namespace gfx;
 #define LCD_BL_HIGH true
 
 // use the default pins for the SPI bus
-using bus_t = tft_spi<HOST,LCD_CS>;
+using bus_t = tft_spi<HOST,LCD_CS,SPI_MODE0,320*240*2+8,2>;
+
 // set up the display
 using lcd_t = ili9341<LCD_DC,LCD_RST,LCD_BL,bus_t,LCD_ROTATION,LCD_BL_HIGH>;
-// set up the touch driver
-using touch_t = xpt2046<TOUCH_CS>;
 
 // declare the display
 lcd_t lcd;
+
+// set up the touch driver
+using touch_t = xpt2046<TOUCH_CS>;
+
 // declare the touch. The touch takes a reference to an SPIClass
 // However, devices that share a bus must share the same instance.
 // Always retrieving the SPIClass from spi_container ensures the same
@@ -69,7 +73,7 @@ size_t font_buffer_len;
 
 // holds the currently selected hue value
 float current_hue;
-  
+float current_gsc;
 // calibrates the screen, optionally writing the calibration file to SPIFFS
 void calibrate(bool write=true) {
   touch.initialize();
@@ -90,7 +94,7 @@ void calibrate(bool write=true) {
   open_font::open(&cbs,&fnt);
   float scale = fnt.scale(30);
   const char* text = "Touch the corners\nas indicated";
-  ssize16 fsz = fnt.measure_text({32767,32767},{0,0},text,scale).inflate(2,2);
+  ssize16 fsz = fnt.measure_text(ssize16::max(),spoint16::min(),text,scale).inflate(2,2);
   srect16 tr = fsz.bounds().center((srect16)lcd.bounds());
   draw::text(lcd,tr,{0,0},text,fnt,scale,color_t::black);
 
@@ -169,29 +173,46 @@ bool read_calibration() {
   return false;
 }
 // draw a 90deg linear gradient from HSV(0%,100%,100%) to HSV(100%,100%,100%)
-void draw_hue_bar(rect16 rect) {
+void draw_hue_bar(const rect16& rect) {
   int w = (float)rect.width()/
           (float)((hsv_pixel<24>::channel_by_name<channel_name::H>::max
                   -hsv_pixel<24>::channel_by_name<channel_name::H>::min)+1);
   if(w==0)
     w=1;
-
-  for(int x = rect.left();x <= rect.right(); ++x) {
-    hsv_pixel<24> px(true,(((float)(x-rect.left()))/(rect.width()-1)),1,1);
-    draw::filled_rectangle(lcd,srect16(x,rect.top(),x+w-1,rect.bottom()),px);
+  rect16 rect2(rect.left(),rect.top(),rect.right(),rect.y1+(rect.y2-rect.y1)/2);
+  for(int x = rect2.x1;x <= rect2.x2; ++x) {
+    hsv_pixel<24> px(true,(((float)(x-rect2.x1))/(rect2.width()-1)),1,1);
+    draw::filled_rectangle(lcd,srect16(x,rect2.top(),x+w-1,rect2.bottom()),px);
+  }
+  rect2.offset_inplace(0,rect2.height());
+  for(int x = rect2.x1;x <= rect2.x2; ++x) {
+    gsc_pixel<16> px(true,(((float)(x-rect2.x1))/(rect2.width()-1)));
+    draw::filled_rectangle(lcd,srect16(x,rect2.top(),x+w-1,rect2.bottom()),px);
   }
 }
 // draw the color match bar (exact and nearest x11 color)
 void draw_color(hsv_pixel<24> color) {
-  draw::filled_rectangle(lcd,srect16(0,140,159,159),color);
+  draw::filled_rectangle(lcd,srect16(0,110,159,129),color);
   x11_t pal;
   typename x11_t::pixel_type px;
   typename x11_t::mapped_pixel_type cpx;
   convert(color,&cpx);
   pal.nearest(cpx,&px);
   pal.map(px,&cpx);
-  draw::filled_rectangle(lcd,srect16(160,140,319,159),cpx);
+  draw::filled_rectangle(lcd,srect16(160,110,319,129),cpx);
 }
+// draw the color match bar (exact and nearest x11 color)
+void draw_color(gsc_pixel<8> color) {
+  draw::filled_rectangle(lcd,srect16(0,110,159,129),color);
+  x11_t pal;
+  typename x11_t::pixel_type px;
+  typename x11_t::mapped_pixel_type cpx;
+  convert(color,&cpx);
+  pal.nearest(cpx,&px);
+  pal.map(px,&cpx);
+  draw::filled_rectangle(lcd,srect16(160,110,319,129),cpx);
+}
+
 // draw the name of the color
 void draw_color_name(hsv_pixel<24> color) {
   x11_t pal;
@@ -206,21 +227,47 @@ void draw_color_name(hsv_pixel<24> color) {
   // attempt to open the font (already checked in setup)
   open_font::open(&cbs,&fnt);
   float scale = fnt.scale(30);
-  ssize16 fsz = fnt.measure_text({32767,32767},{0,0},name,scale).inflate(2,2);
-  srect16 tr = fsz.bounds().center({0,160,319,208});
-  draw::filled_rectangle(lcd,srect16(0,160,319,208),color_t::white);
+  ssize16 fsz = fnt.measure_text(ssize16::max(),spoint16::zero(),name,scale).inflate(2,2);
+  srect16 tr = fsz.bounds().center({0,130,319,179});
+  draw::filled_rectangle(lcd,srect16(0,130,319,179),color_t::white);
+  draw::text(lcd,tr,{0,0},name,fnt,scale,color_t::black);
+}
+// draw the name of the color
+void draw_color_name(gsc_pixel<8> color) {
+  x11_t pal;
+  typename x11_t::pixel_type ipx;
+  typename x11_t::mapped_pixel_type cpx;
+  convert(color,&cpx);
+  pal.nearest(cpx,&ipx);
+  const char* name = x11_names[ipx.template channel<0>()];
+  // reconstitute our font stream from PSRAM
+  const_buffer_stream cbs(font_buffer,font_buffer_len);
+  open_font fnt;
+  // attempt to open the font (already checked in setup)
+  open_font::open(&cbs,&fnt);
+  float scale = fnt.scale(30);
+  ssize16 fsz = fnt.measure_text(ssize16::max(),spoint16::zero(),name,scale).inflate(2,2);
+  srect16 tr = fsz.bounds().center({0,130,319,179});
+  draw::filled_rectangle(lcd,srect16(0,130,319,179),color_t::white);
   draw::text(lcd,tr,{0,0},name,fnt,scale,color_t::black);
 }
 
-void draw_frame(float hue) {
+void draw_frame() {
+  if(current_gsc==current_gsc) {
+    gsc_pixel<8> gpx(true,current_gsc);
+    draw::filled_rectangle(lcd,rect16(0,0,319,109),gpx);
+    draw_color(gpx);
+    draw_color_name(gpx);
+    return;
+  }
   // draw a linear gradient on the HSV axis, where h is fixed at "hue"
   // and S and V are along the Y and X axes, respectively
-  hsv_pixel<24> px(true,hue,1,1);
+  hsv_pixel<24> px(true,current_hue,1,1);
   auto px2 = px;
   // batching is the fastest way
-  auto b = draw::batch(lcd,srect16(0,0,319,139));
-  for(int y = 0;y<140;++y) {
-    px2.template channelr<channel_name::S>(((double)y)/139.0);
+  auto b = draw::batch(lcd,srect16(0,0,319,109));
+  for(int y = 0;y<110;++y) {
+    px2.template channelr<channel_name::S>(((double)y)/109.0);
     for(int x = 0;x<320;++x) {
       px2.template channelr<channel_name::V>(((double)x)/319.0);
       b.write(px2);
@@ -278,10 +325,11 @@ void setup() {
     calibrate(true);
   }
   current_hue = 0;
+  current_gsc = NAN;
   // draw the hue bar at the bottom
-  draw_hue_bar({0,210,319,239});
+  draw_hue_bar({0,180,319,239});
   // draw the initial frame 
-  draw_frame(0);
+  draw_frame();
 }
 
 void loop() {
@@ -289,10 +337,19 @@ void loop() {
   // touched?
   if(touch.calibrated_xy(&x,&y)) {
     // hue bar?
-    if(y>=210) {
-      current_hue = ((double)x)/319.0;
-      draw_frame(current_hue);
-    } else if(y<140) { // gradient area
+    if(y>=180) {
+        if(y<210) {
+          current_gsc = NAN;
+          current_hue = ((double)x)/319.0;
+          draw::filled_rectangle(lcd,srect16(0,0,319,179),hsv_pixel<24>(true,current_hue,1,1));
+          draw_frame();
+        } else {
+          current_hue = NAN;
+          current_gsc = ((double)x)/319.0;
+          draw::filled_rectangle(lcd,srect16(0,0,319,179),gsc_pixel<8>(true,current_gsc));
+          draw_frame();
+        }
+    } else if(y<140 && current_hue==current_hue) { // gradient area
       double s = ((double)y)/139.0;
       double v = ((double)x)/319.0;
       // get our HSV pixel
